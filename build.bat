7d11f6a1b9af2ddb898ee265ecac6d5109e05d1c
Nori Silva <norivan.oliveira@gmail.com>
2026-08-01 01:29:13 -0300

    chore: update build.bat to generate standalone exe and update gitignore

--- build.bat
+++ build.bat
@@ -6,9 +6,14 @@ python -m playwright install chromium
 
 for /f "delims=" %%i in ('python -c "import playwright; import os; print(os.path.join(os.path.dirname(playwright.__file__), 'driver', 'package', '.local-browsers'))"') do set P_BROWSERS=%%i
 
+echo Limpando builds anteriores...
+if exist build rd /s /q build
+if exist dist rd /s /q dist
+if exist F1SetupsAssist.spec del /q F1SetupsAssist.spec
+
 echo Compilando o F1 Setups Assist para Executavel (.exe)...
-python -m PyInstaller --noconfirm --onedir --windowed --name "F1SetupsAssist" --add-data "data;data" --add-data "overlay;overlay" --add-data "scraper;scraper" --add-data "transformer;transformer" --add-data "%P_BROWSERS%;playwright\driver\package\.local-browsers" --icon=NONE main.py
+python -m PyInstaller --clean --noconfirm --onefile --windowed --name "F1SetupsAssist" --add-data "data;data" --add-data "overlay;overlay" --add-data "scraper;scraper" --add-data "transformer;transformer" --add-data "%P_BROWSERS%;playwright\driver\package\.local-browsers" --icon=NONE main.py
 
 echo Build concluido!
-echo O executavel esta na pasta "dist/F1SetupsAssist/F1SetupsAssist.exe"
+echo O executavel esta na pasta "dist/F1SetupsAssist.exe"
 pause
